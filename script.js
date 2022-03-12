b3b86251bea0df7a119a42b162201bcf4da5e4c8
shelmaimara3 <86011698+shelmaimara3@users.noreply.github.com>
2022-03-12 23:48:42 +0700

    webprofile
    saya mengirim semua file

---
+++ script.js
@@ -0,0 +1,43 @@
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector("nav ul");
+
+navToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+let line = document.getElementById("words");
+let txts = ["a scholar", "a scriptwriter", "a web developer"];
+let speed = 50;
+
+async function typewriter(txt) {
+  for (let i = 0; i < txt.length; i++) {
+    line.innerHTML += txt[i];
+    await delay(speed);
+  }
+}
+
+async function reverseTypewriter(txt) {
+  for (let i = txt.length; i > 0; i--) {
+    line.innerHTML = line.innerHTML.slice(0, -1);
+    await delay(speed);
+  }
+}
+async function writeLoop() {
+  for (let i = 0; i < txts.length; i++) {
+    await typewriter(txts[i]);
+    await delay(1000);
+    await reverseTypewriter(txts[i]);
+    await delay(300);
+  }
+  writeLoop();
+}
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+writeLoop();
